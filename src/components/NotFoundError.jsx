736779cd1204884763e0c8503a7b098c676c5d69
Square <aayushk4980@gmail.com>
2024-07-01 20:38:43 +0530

    Fixed NotFoundError.jsx word bug

--- src/components/NotFoundError.jsx
+++ src/components/NotFoundError.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function NotFoundError({ wordData }) {
+
+  // Getting word from route URL.
+  const { searchWordParam } = useParams();
 
   return (
     <div className='error'>
         <p className='errorTitle'>{wordData["title"]}</p>
         <p className='errorDescription'>{wordData["message"]}</p>
         <p className='errorResolution'>{wordData["resolution"]}</p>
-        <a className='linkToGoogle' href={`https://www.google.com/search?q=${wordData.title}%20meaning`} target='_blank'>Find on Google - {wordData.title}</a>
+        <a className='linkToGoogle' href={`https://www.google.com/search?q=${searchWordParam}%20meaning`} target='_blank'>Find on Google - {searchWordParam}</a>
     </div>
   );
 };
