f42eae14c083ac4ddd7fca0e1caa90d60abd19c8
Aayush259 <Aayush259@users.noreply.github.com>
2024-07-18 12:41:56 +0000

    deploy: de53b9ec5908efd8b815a7fed565a0bfe38f085c

---
+++ assets/WordDataContext-AGhzP4WL.js
@@ -0,0 +1,1 @@
+import{r as t,j as s}from"./index-CW8Z3jpq.js";const r=t.createContext(),n=({children:o,value:e})=>s.jsx(r.Provider,{value:e,children:o}),x=()=>t.useContext(r);export{n as W,x as u};
--- assets/WordDataContext-C9fQhKKE.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as s}from"./index-Dmm7mE3a.js";const r=t.createContext(),n=({children:o,value:e})=>s.jsx(r.Provider,{value:e,children:o}),x=()=>t.useContext(r);export{n as W,x as u};
