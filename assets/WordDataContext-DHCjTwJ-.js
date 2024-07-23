b1ed8f74bd033698d2a9f25d6c00f794bf9b097b
Aayush259 <Aayush259@users.noreply.github.com>
2024-07-23 06:50:20 +0000

    deploy: 22849c7083c6925ab7563779cc03cd09e7d0a295

---
+++ assets/WordDataContext-DHCjTwJ-.js
@@ -0,0 +1,1 @@
+import{r as t,j as s}from"./index-Bb8q72aI.js";const r=t.createContext(),n=({children:o,value:e})=>s.jsx(r.Provider,{value:e,children:o}),x=()=>t.useContext(r);export{n as W,x as u};
