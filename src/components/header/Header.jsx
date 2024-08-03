c4b673dd940f437c98cfe535ccd7524cbfe12281
Square <aayushk4980@gmail.com>
2024-08-03 21:42:51 +0530

    Added tailwind classes to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import photo from '../../images/photo.jpeg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Header({theme, changeTheme}) {
+export default function Header() {
 
     return (
-        <header className={`header flex ${theme}`}>
-            <h1>Dictionary</h1>
-            <div className='flex'>
-                <button className='changeThemeBtn' onClick={() => changeTheme()}>
-                    <FontAwesomeIcon icon="fa-solid fa-moon" style={{fontSize: '20px', transform: 'rotate(-25deg)'}} />
+        <header
+            className="flex flex-wrap w-full items-center justify-between p-4 bg-violet-300 rounded-lg"
+        >
+            <h1
+                className="text-2xl sm:text-3xl font-semibold"
+            >
+                Dictionary
+            </h1>
+
+            <div
+                className="flex items-center justify-center gap-3"
+            >
+                <button>
+                    <FontAwesomeIcon
+                        icon="fa-solid fa-moon"
+                        className="text-xl -rotate-[25deg]"
+                    />
                 </button>
-                <img src={photo} alt="Author" height={40} />
+
+                <img
+                    src={photo}
+                    alt="Author"
+                    className="h-10 rounded-full"
+                />
             </div>
         </header>
     );
-};
-
-Header.propTypes = {
-    theme: PropTypes.string.isRequired,
-    changeTheme: PropTypes.func.isRequired,
 };
