bda050b3b5bf62887e346b7f443ace294e3c00c8
Square <aayushk4980@gmail.com>
2024-08-04 10:53:26 +0530

    Added tailwind classes for loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 export default function Loader() {
   return (
-    <div className='loaderContainer flex'>
+    <div
+      className="flex flex-row items-center justify-center h-[70vh]"
+    >
       {
-        Array.from({length: 3}, ((_, index) => (
-          <div key={index} className="circle"></div>
-        )))
+        ['animate-loading1', 'animate-loading2', 'animate-loading3'].map(animation => (
+          <div
+            key={animation}
+            className={`w-3 h-3 m-1 rounded-full bg-slate-900 opacity-0 ${animation}`}
+          ></div>
+        ))
       }
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        loading: {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '50%': { transform: 'scale(0)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        }
+      },
+      animation: {
+        loading1: 'loading 1s infinite',
+        loading2: 'loading 1s infinite 0.2s',
+        loading3: 'loading 1s infinite 0.4s',
+      }
+    },
   },
   plugins: [],
 }
