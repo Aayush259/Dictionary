2c947899b1781e728b1be75c3cfc07cbc8e68a74
Square <aayushk4980@gmail.com>
2024-08-06 11:41:42 +0530

    Completely removed context api and added redux

--- src/app/middleware/historyMiddleWare.js
+++ src/app/middleware/historyMiddleWare.js
@@ -1,10 +1,13 @@
 
 const historyMiddleWare = store => next => action => {
+    // Call reducer.
     const result = next(action);
 
     if (action.type.startsWith('history')) {
+        // Getting history state from store.
         const historyState = store.getState().history.value;
 
+        // Updating history in local storage.
         localStorage.setItem('userDictHistory', JSON.stringify(historyState));
     };
 
--- src/components/main/MainSection.jsx
+++ src/components/main/MainSection.jsx
@@ -1,19 +1,16 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { WordDataContextProvider } from '../../contexts/WordDataContext.jsx';
 import NavLinks from './NavLinks.jsx';
 import Loader from '../Loader.jsx';
 
 export default function MainSection() {
 
     return (
-        <WordDataContextProvider>
-            <main>
-                <NavLinks />
-                <Suspense fallback={<Loader />}>
-                    <Outlet />
-                </Suspense>
-            </main>
-        </WordDataContextProvider>
+        <main>
+            <NavLinks />
+            <Suspense fallback={<Loader />}>
+                <Outlet />
+            </Suspense>
+        </main>
     );
 };
